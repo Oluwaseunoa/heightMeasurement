c0190ddf7ad9fdb322f252a3c6f099a5a2fb5bf0
oluwaseunoa <oluwaseun.beicks@gmail.com>
2022-12-03 12:01:22 +0100

    Add project files.

---
+++ heightMeasurement/Program.cs
@@ -0,0 +1,29 @@
+﻿class  Program
+{
+    static void Main()
+    {
+        int heightInInches = 75; //input
+        double heightInCentimeter = heightInInches * 2.54; //inch in cent = 2.54;
+        string result = null; //initializing result
+
+        //if statement
+        if(heightInCentimeter < 150)
+        {
+            result = "Dwarf";
+        }
+        else if(heightInCentimeter >= 150 && heightInCentimeter <= 165)
+        {
+            result = "Average Height";
+        }
+        else if(heightInCentimeter >= 165 && heightInCentimeter <= 195)
+        {
+            result = "Tall";
+        }
+        else
+        {
+            result = "Abnormal Height";
+        }
+        System.Console.WriteLine(result);
+        System.Console.ReadKey();
+    }
+}
